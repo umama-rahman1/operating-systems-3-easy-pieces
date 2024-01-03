a43ccf349421e1546a0d975f44b37f897d25b18d
mepha89 <umamarahman12@gmail.com>
2024-01-03 17:08:10 -0330

    ch-5-q2

--- ch-5-process-api/homework-code/process_api_2.c
+++ ch-5-process-api/homework-code/process_api_2.c
@@ -7,5 +7,22 @@
 
 int main(int argc, char *argv[])
 {
-    
+    // Write a program that opens a file (with the open() system call) and then calls fork() to create a new process. Can both the child and parent access the file descriptor returned by open()? What happens when they are writing to the file concurrently, i.e., at the same time?
+    int fd = open("./q2-output.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+    int rc = fork();
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    { // child
+        char *child_str = "This is child string.\n";
+        write(fd, child_str, strlen(child_str));
+    }
+    else
+    { // parent
+        char *parent_str = "This is parent string.\n";
+        write(fd, parent_str, strlen(parent_str));
+    }
 }
