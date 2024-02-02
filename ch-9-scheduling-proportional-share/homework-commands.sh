2a81de97dc5c35bf1c73de17c740ca596cb3e190
mepha89 <umamarahman12@gmail.com>
2024-02-01 21:38:06 -0500

    ch-9-homework-graphs

---
+++ ch-9-scheduling-proportional-share/homework-commands.sh
@@ -0,0 +1,20 @@
+# Q1
+python ./lottery.py -s 1 -j 3 -c
+python ./lottery.py -s 2 -j 3 -c
+python ./lottery.py -s 3 -j 3 -c
+
+# Q2
+python ./lottery.py -l 10:1,10:100 -c
+
+# Q3
+python ./lottery.py -s 0 -l 100:100,100:100 -c
+U = 192/200 = 0.96
+
+# Q4
+ python ./plot_quantum.py
+
+ # Q5
+python ./plot.py
+
+# Q6
+python ./plot_stride.py
